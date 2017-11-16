15b5c9bd1384c872e7669437535e67093af35e27
Anže Marinko <amarinko@skavt.net>
2017-11-16 12:18:21 +0100

    pregledovanje kode

--- razvrscanje.py
+++ razvrscanje.py
@@ -1,6 +1,8 @@
+# Dinamika okuzb in imunizacije
+
 import numpy as np
 
-x = [1,2]
+x = [0.95, 0.05]
 natancnost = 1
 interval_0_1 = [i / 10**natancnost for i in range(10**natancnost + 1)]
 
@@ -13,6 +15,7 @@ for tocka in range(10**natancnost,(10**natancnost + 1)**len(x) + 1):
         vsotakomp = vsotakomp//(10**natancnost + 1)
     if sum(koordinate) == 1:
         Delta.append(koordinate)
+# Delta je simpleks (sum_{i=1:n}x_i=1, x_i>=0 za i=1:n)
 
 A = [[0 for i in range(len(x))] for j in range(len(x))]
 for i in range(len(x)):
@@ -32,15 +35,15 @@ def Y(x, A = A, Delta = Delta):
 def S(x):
     y = Y(x)
     if len(y) != 0:
-        return y[0]
+        return y
     return x
 
 def delta(y,x,A=A):
     x = np.array(x)
     y = np.array(y)
     A = np.array(A)
-    mx = sum(sum((y - x) * A * np.transpose([x])))
-    my = sum(sum(np.transpose((y - x) * A) * y))
+    mx = np.dot(np.dot((y - x), A), x)
+    my = np.dot(np.dot((y - x), A), y)
     if my-mx < 0:
         return min(-mx/(my-mx),1)
     return 1
