229e8ce9f5b42d8ccd0b36eeb12de3ee5df29763
AccipitrisArti <amarinko@skavt.net>
2017-11-17 12:50:06 +0100

    predelava kode

--- razvrscanje.py
+++ razvrscanje.py
@@ -3,33 +3,32 @@
 import numpy as np
 import random
 
-x = [0.1, 0.5, 0.4]
+w = [0.1, 0.8, 0.1]
 natancnost = 2
-interval_0_1 = [i / 10**natancnost for i in range(10**natancnost + 1)]
 
-Delta = []
-for tocka in range(10**natancnost,(10**natancnost + 1)**len(x) + 1):
-    vsotakomp = tocka
-    koordinate = [0] * len(x)
-    for komponenta in range(len(x)):
-        koordinate[komponenta] = (vsotakomp%(10**natancnost + 1))/10**natancnost
-        vsotakomp = vsotakomp//(10**natancnost + 1)
-    if sum(koordinate) == 1:
-        Delta.append(koordinate)
+def Delta():
+    koordinate = [random.randint(0,10**natancnost + 1) for i in range(len(w))]
+    vsota = sum(koordinate)
+    if vsota != 0:
+        for k in range(len(koordinate)):
+            koordinate[k] *= 1/vsota
+    else:
+        return [0,0,0]
+    return koordinate
 # Delta je simpleks (sum_{i=1:n}x_i=1, x_i>=0 za i=1:n)
 
-A = [[0 for i in range(len(x))] for j in range(len(x))]
-for i in range(len(x)):
-    for j in range(len(x)):
-        A[i][j] = (abs(x[i] - x[j]))**(1/2)
+A = [[0 for i in range(len(w))] for j in range(len(w))]
+for i in range(len(w)):
+    for j in range(len(w)):
+        A[i][j] = (abs(w[i] - w[j]))**(1/2)
 # hitrost konvergence in limita odvisna od definicije a_{i,j}
 
-def Y(x, A = A, Delta = Delta):
+def Y(x, A = A):
     x = np.array(x)
     A = np.array(A)
     Gamma = []
-    for y in Delta:
-        y = np.array(y)
+    while len(Gamma) == 0:
+        y = Delta()
         z = [y[i]-x[i] for i in range(len(x))]
         if np.dot(np.dot(z, A), x) > 0:
            Gamma.append(y)
@@ -50,10 +49,9 @@ def delta(y,x,A=A):
         return min(-mx/(my-mx),1)
     return 1
 
-pop = x
+pop = w
 for t in range(300):
     s = np.array(S(pop))
     pop = np.array(pop)
     pop += delta(s,pop) * (s-pop)
-    if t%30 == 0:
-        print(pop)
+    print(pop)
